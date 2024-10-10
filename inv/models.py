c68870d0a718f0bb836c3a2766cfc11ec5c3a9ba
Soria bol <ejsbsoria14@gmail.com>
2024-10-09 23:08:09 -0600

    cree templates

--- inv/models.py
+++ inv/models.py
@@ -1,5 +1,5 @@
+from typing import Iterable
 from django.db import models
-
 from bases.models import ClaseModelo
 
 class Categoria(ClaseModelo):
@@ -9,12 +9,94 @@ class Categoria(ClaseModelo):
         unique=True
     )
 
-    def __srt__(self):
-        return'{}'.format(self.descripcion)
-    
-        def save(self):
-            self.descripcion = self.descripcion.upper() 
-            super(Categoria, self).save()
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+
+    def save(self, *args, **kwargs):
+        self.descripcion = self.descripcion.upper()
+        super(Categoria, self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name_plural="Categorias" 
+        verbose_name_plural = "Categorias"
+
+class SubCategoria(ClaseModelo):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripción de la subcategoría',
+    )
+
+    def __str__(self):
+        return '{}: {}'.format(self.categoria.descripcion, self.descripcion)
+
+    def save(self, *args, **kwargs):
+        self.descripcion = self.descripcion.upper()
+        super(SubCategoria, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = "Sub Categorías"
+        constraints = [
+            models.UniqueConstraint(fields=['categoria', 'descripcion'], name='unique_categoria_descripcion')
+        ]
+
+class Marca(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripción de la Marca',
+        unique=True
+    )
+
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+    
+    def save(self, *args, **kwargs):
+        self.descripcion = self.descripcion.upper()
+        super(Marca, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = "Marcas"
+
+
+
+class UnidadMedida(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripcion de la Unidad Medida',
+        unique=True
+    )
+
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+    
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(UnidadMedida, self).save()
+
+    class Meta:
+        verbose_name_plural = "Unidades de Medida"
+        
+class Producto(ClaseModelo):
+    codigo = models.CharField(
+        max_length = 20,
+        unique=True
+    )
+    codigo_barra = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=200)
+    precio = models.FloatField(default=0)
+    existencia = models.IntegerField(default=0)
+    ultima_compra = models.DateField(null=True, blank=True)
+
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    unidad_medida = models.ForeignKey(UnidadMedida, on_delete=models.CASCADE)
+    subcategoria = models.ForeignKey(SubCategoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(Producto,self).save()
+
+    class Meta:
+        verbose_name_plural = "Productos"
+        unique_together = ('codigo','codigo_barra')
--- inv/urls.py
+++ inv/urls.py
@@ -1,11 +1,39 @@
 from django.urls import path
 
-from .views import CategoriaView, CategoriaNew, CategoriaEdit, CategoriaDel
+from .views import CategoriaView, CategoriaNew, CategoriaEdit, CategoriaDel,\
+    SubCategoriaView, SubCategoriaNew,SubCategoriaEdit,SubCategoriaDel,\
+    MarcaView, MarcaNew, MarcaEdit, marca_inactivar, \
+    UMView,UMNew,UMEdit,um_inactivar, \
+    ProductoView,ProductoNew,ProductoEdit,producto_inactivar
+    
+    
 
 urlpatterns = [
     path('categorias/', CategoriaView.as_view(), name='categoria_list'),
     path('categorias/new', CategoriaNew.as_view(), name='categoria_new'),
     path('categorias/edit/<int:pk>', CategoriaEdit.as_view(), name='categoria_edit'),   
-    path('categorias/delete/<int:pk>', CategoriaDel.as_view(), name='categoria_del'),   
+    path('categorias/delete/<int:pk>', CategoriaDel.as_view(), name='categoria_del'),
+#subcategorias
+    path('subcategorias/', SubCategoriaView.as_view(), name='subcategoria_list'),   
+    path('subcategorias/new', SubCategoriaNew.as_view(), name='subcategoria_new'),
+    path('subcategorias/edit/<int:pk>',SubCategoriaEdit.as_view(), name='subcategoria_edit'), 
+    path('subcategorias/delete/<int:pk>', SubCategoriaDel.as_view(), name='subcategoria_del'),
+#marcas
+    path('marcas/', MarcaView.as_view(), name='marca_list'), 
+    path('marcas/new', MarcaNew.as_view(), name='marca_new'),
+    path('marcas/edit/<int:pk>',MarcaEdit.as_view(), name='marca_edit'),
+    path('marcas/inactivar/<int:id>',marca_inactivar, name='marca_inactivar'),
+#unidades de medida
+    path('UnidadesMedidas/', UMView.as_view(), name='um_list'), 
+    path('UnidadesMedidas/new', UMNew.as_view(), name='um_new'),
+    path('UnidadesMedidas/edit/<int:pk>',UMEdit.as_view(), name='um_edit'),
+    path('UnidadesMedidas/inactivar/<int:id>',um_inactivar, name='um_inactivar'),
+#producto url
+    path('productos/', ProductoView.as_view(), name='producto_list'), 
+    path('productos/new', ProductoNew.as_view(), name='producto_new'),
+    path('productos/edit/<int:pk>',ProductoEdit.as_view(), name='producto_edit'),
+    path('productos/inactivar/<int:id>',producto_inactivar, name='producto_inactivar'),
+
+
 
 ]
