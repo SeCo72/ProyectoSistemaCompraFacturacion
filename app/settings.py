d434c6bcf0485757988e90e7b99e249b7b7084b6
by SeCo <selvinptiul@outlook.es>
2024-11-04 06:21:20 -0600

    :card_file_box: Configuración Heroku

--- app/settings.py
+++ app/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+from decouple import config
+
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b!=ew!cl7326rr5$6kwam0tnu=-43jikbqax^_)cf=1ecfc^bv'
+#SECRET_KEY = 'django-insecure-b!=ew!cl7326rr5$6kwam0tnu=-43jikbqax^_)cf=1ecfc^bv'
+SECRET_KEY=config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com"]
 
 
 # Application definition
@@ -55,7 +60,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_userforeignkey.middleware.UserForeignKeyMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -128,9 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 LOGIN_URL = '/login/'  # Asegúrate de que esto coincida con la ruta de tu vista de inicio de sesión
 LOGIN_REDIRECT_URL ='/'
