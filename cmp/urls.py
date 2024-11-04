007ea778fae65ed797f8dd750e03a0877b474ff4
David Catun <bicoc@miumg.edu.gt>
2024-11-03 23:36:03 -0600

    :sparkles: se agregaro rutas para: ccompras edit, compras del y compar print all

--- cmp/urls.py
+++ cmp/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
     path('compras/edit/<int:compra_id>',compras, name="compras_edit"),
     path('compras/<int:compra_id>/delete/<int:pk>',CompraDetDelete.as_view(), name="compras_del"),
 
-     path('compras/listado', reporte_compras, name='compras_print_all'),
-     path('compras/<int:compra_id>/imprimir', imprimir_compra,name="compras_print_one"),
+        path('compras/listado', reporte_compras, name='compras_print_all'),
+        path('compras/<int:compra_id>/imprimir', imprimir_compra,name="compras_print_one"),
 ]
 
 
